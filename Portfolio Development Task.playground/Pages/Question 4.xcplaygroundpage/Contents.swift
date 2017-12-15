b0ae341ea944a794b0448af071c43afe01cd5f47
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-12-15 12:27:36 -0500

    finished question 4

--- Portfolio Development Task.playground/Pages/Question 4.xcplaygroundpage/Contents.swift
+++ Portfolio Development Task.playground/Pages/Question 4.xcplaygroundpage/Contents.swift
@@ -19,7 +19,11 @@
  
  */
 // Answer question 4 below
-
+var moneyIHaveEarned = 0.0
+moneyIHaveEarned += 5
+moneyIHaveEarned -= 2
+moneyIHaveEarned *= 4
+moneyIHaveEarned += (0.75*13)
 /*:
  ## Now share your understanding
  
